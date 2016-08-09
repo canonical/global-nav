6d08df09f545e0fff334150e7c4af2067ae1f039
Anthony Dillon <me@anthonydillon.com>
2016-08-10 00:50:11 +0100

    Global is working

--- src/js/global.js
+++ src/js/global.js
@@ -1,5 +1,4 @@
-
-var ai = [{ url: "http://www.ubuntu.com", title:"Ubuntu" },
+let ai = [{ url: "http://www.ubuntu.com", title:"Ubuntu" },
 { url: "http://community.ubuntu.com/", title:"Community" },
 { url: "http://askubuntu.com", title:"Ask!" },
 { url: "http://developer.ubuntu.com", title:"Developer" },
@@ -11,13 +10,13 @@ var ai = [{ url: "http://www.ubuntu.com", title:"Ubuntu" },
 { url: "http://partners.ubuntu.com", title:"Partners" },
 { url: "https://buy.ubuntu.com/", title:"Shop" }];
 
-var more = [{ url: "https://help.ubuntu.com", title:"Help" },
+let more = [{ url: "https://help.ubuntu.com", title:"Help" },
 { url: "http://ubuntuforums.org", title:"Forum" },
 { url: "http://www.launchpad.net", title:"Launchpad" },
 { url: "http://shop.ubuntu.com", title:"Merchandise" },
 { url: "http://www.canonical.com", title:"Canonical" }];
 
-var global = {};
+const global = {};
 
 global.setup = function () {
     this.addNav(this.getNav());
@@ -25,12 +24,12 @@ global.setup = function () {
 };
 
 global.getNav = function() {
-    var begin = '<nav role="navigation" id="nav-global"><div class="nav-global-wrapper"><ul class="nav-global-main">';
-    var end = '</ul></div></nav>';
-    var mu = '';
-    var i = 0;
+    let begin = '<nav role="navigation" id="nav-global"><div class="nav-global-wrapper"><ul class="nav-global-main">';
+    let end = '</ul></div></nav>';
+    let mu = '';
+    let i = 0;
     while (i < ai.length) {
-        mu += '<li><a href="' + ai[i].url + '" ' + core.getActive(ai[i].url) + '>' + ai[i].title + '</a></li>';
+        mu += '<li><a href="' + ai[i].url + '" ' + this.getActive(ai[i].url) + '>' + ai[i].title + '</a></li>';
         i++;
     }
     i = 0;
@@ -45,41 +44,43 @@ global.getNav = function() {
 };
 
 global.addNav = function(mu) {
-    var gI = 'body';
-    if (core.globalPrepend) {
-        gI = core.globalPrepend;
+    let gI = 'body';
+
+    if (this.globalPrepend) {
+        gI = this.globalPrepend;
     }
 
-    YUI().use('node', function(Y) {
-        Y.one(gI).prepend(mu);
-        var ml = Y.one('#nav-global .more');
-        if (ml){
-            ml.on('click',function(e) {
-                e.stopPropagation();
-                e.preventDefault();
-                this.toggleClass('open');
-                return false;
-            });
-            ml.one('span').on('click',function(e) {
-                e.preventDefault();
-                e.stopPropagation();
-            });
-            ml.one('ul').on('click',function(e) {
-                e.stopPropagation();
-            });
-        }
-        Y.one('body').on('click', function(e){
-            var ml = Y.one('#nav-global .more');
-            if (ml.hasClass('open')) {
-                ml.removeClass('open');
-            }
+    document.querySelector(gI).innerHTML = mu;
+    let ml = document.querySelector('#nav-global .more');
+    if (ml){
+        ml.addEventListener('click',function(e) {
+            e.stopPropagation();
+            e.preventDefault();
+            ml.classList.toggle('open');
+            return false;
         });
+
+        ml.querySelector('span').addEventListener('click',function(e) {
+            e.preventDefault();
+            e.stopPropagation();
+        });
+
+        ml.querySelector('ul').addEventListener('click',function(e) {
+             e.stopPropagation();
+        });
+    }
+
+    document.body.addEventListener('click', function(e) {
+        let ml = document.querySelector('#nav-global .more');
+        if (ml.classList.contains('open')) {
+            ml.classList.remove('open');
+        }
     });
 };
 
 global.getActive = function(link) {
-    var fullurl = this.getURL();
-    var url = fullurl.substr(0,link.length);
+    let fullurl = this.getURL();
+    let url = fullurl.substr(0,link.length);
 
     if (link == 'http://www.ubuntu.com') {
         if (fullurl.substr(0,35) == 'http://www.ubuntu.com/certification' ) {
@@ -91,25 +92,23 @@ global.getActive = function(link) {
 
 
 global.getURL = function(){
-    var url = document.URL;
+    let url = document.URL;
     url = url.replace('https://developer.ubuntu.com','http://developer.ubuntu.com');
     return url;
 };
 
 global.trackClicks = function() {
-    YUI().use('node', function(Y) {
-        Y.all('#nav-global a').on('click',function(e) {
-            e.preventDefault();
-            try {
-                _gaq.push(['_trackEvent', 'Global bar click', e.target.get('text'), core.getURL()]);
-            } catch(err){}
-            setTimeout(function() {
-                document.location.href = e.target.get('href');
-            }, 100);
-        });
+    document.querySelector('#nav-global a').addEventListener('click',function(e) {
+        e.preventDefault();
+        try {
+            _gaq.push(['_trackEvent', 'Global bar click', e.target.get('text'), core.getURL()]);
+        } catch(err) {}
+        setTimeout(function() {
+            document.location.href = e.target.get('href');
+        }, 100);
     });
 };
 
 if (!global.globalPrepend) {
-    this.setup();
+    global.setup();
 }
