258db9d2aad18cc97ff4c9ed3b60303884f3a35c
Anthony Dillon <me@anthonydillon.com>
2019-11-14 11:54:26 +0000

    Lint the JS

--- src/js/global-nav.js
+++ src/js/global-nav.js
@@ -277,7 +277,7 @@ function addListeners(breakpoint, wrapper) {
 }
 
 function validURL(str) {
-  let pattern = new RegExp(
+  const pattern = new RegExp(
     '^(https?:\\/\\/)?' + // protocol
     '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
     '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
